341993f036b13af4c47b8b626646cb4322a5e4f4
Francisco Yang <yangfranci@gmail.com>
2024-05-07 21:15:27 -0400

    8.9 Complete

--- part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Program.java
+++ part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Program.java
@@ -7,4 +7,18 @@ public class Program {
         // Test your program here!
     }
 
+    public static void printValues(HashMap<String, Book> hashmap){
+        for(Book title : hashmap.values()){
+            System.out.println(title);
+        }
+    }
+
+    public static void printValueIfNameContains(HashMap<String, Book> hashmap, String text){
+        for(Book title : hashmap.values()){
+            if(title.getName().contains(text)){
+                System.out.println(title);
+            }
+        }
+    }
+
 }
