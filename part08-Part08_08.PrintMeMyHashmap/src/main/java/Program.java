02fd1f4c805d722da25853e49f1a0b2913f49c20
Francisco Yang <yangfranci@gmail.com>
2024-05-07 20:51:33 -0400

    8.8v2 Complete

--- part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
+++ part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
@@ -7,4 +7,26 @@ public class Program {
         // Test your program here!
     }
 
+    public static void printKeys(HashMap<String, String> hashmap){
+        for(String name : hashmap.keySet()){
+            System.out.println(name);
+        }
+    }
+
+    public static void printKeysWhere(HashMap<String, String> hashmap, String text){
+        for(String name : hashmap.keySet()){
+            if(name.contains(text)){
+                System.out.println(name);
+            }
+        }
+    }
+
+    public static void printValuesOfKeysWhere(HashMap<String, String> hashmap, String text){
+        for(String name : hashmap.keySet()){
+            if(name.contains(text)){
+                System.out.println(hashmap.get(name));
+            }
+        }
+    }
+
 }
