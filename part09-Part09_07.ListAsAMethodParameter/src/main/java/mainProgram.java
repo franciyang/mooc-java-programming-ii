993f969766e17e49a97caed2db37b26d42a56565
Francisco Yang <yangfranci@gmail.com>
2024-05-20 14:15:03 -0400

    9.7 Complete

--- part09-Part09_07.ListAsAMethodParameter/src/main/java/mainProgram.java
+++ part09-Part09_07.ListAsAMethodParameter/src/main/java/mainProgram.java
@@ -1,4 +1,4 @@
-
+import java.util.List;
 
 public class mainProgram {
 
@@ -7,7 +7,7 @@ public class mainProgram {
 
     }
 
-    // Implement here a method returnSize
-    // which returns the size of the list given to it
-    //as a parameter
+    public static int returnSize(List<String> object){
+        return object.size();
+    }
 }
